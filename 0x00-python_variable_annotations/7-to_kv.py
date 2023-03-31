99a3ca03b0ac267d872d5c508b542b03e895107d
shady-cj <petersp2000@gmail.com>
2023-03-31 13:07:19 +0100

    change the annotations to older version by importing the collections and Unions from typing module

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,9 +2,10 @@
 """
 a module that contains a to_kv function
 """
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     returns a tuple of k and v**2
     """
