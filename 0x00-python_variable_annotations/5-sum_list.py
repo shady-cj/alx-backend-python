1881232910a62010fe97a70cc38edd31917bf5e3
shady-cj <petersp2000@gmail.com>
2023-03-31 11:58:16 +0100

    implemented task #5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a module that contains a sum_list function
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    returns the sum of input_list as a float
+    """
+    return sum(input_list)
