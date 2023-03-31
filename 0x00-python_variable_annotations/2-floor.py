8f08186073683e336dc96dbeb5bcb1a7c938cac5
shady-cj <petersp2000@gmail.com>
2023-03-31 11:45:47 +0100

    implementing task #2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a module that contains a floor function
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    returns the floor of n of type float
+    """
+    return math.floor(n)
