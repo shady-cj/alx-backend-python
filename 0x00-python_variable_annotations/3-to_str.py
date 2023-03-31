dab6f4cca1db5bde3ab27c92c80122181d2afc41
shady-cj <petersp2000@gmail.com>
2023-03-31 11:49:22 +0100

    implemented task #3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a module that contains a to_str function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns the string of n of type float
+    """
+    return str(n)
