afacdf383758b76b6e5c98fdb5f26cfd394defd1
shady-cj <petersp2000@gmail.com>
2023-03-31 13:27:02 +0100

    change the annotations to older version by importing the collections and Unions from typing module

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ a module that contains a sum_mixed_list function
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     returns the sum of mxd_list of ints
     and floats as a float
