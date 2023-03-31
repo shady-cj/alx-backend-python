63d1be1702bcd374762c68e213c6c10327641e7e
shady-cj <petersp2000@gmail.com>
2023-03-31 11:42:35 +0100

    implementing task #1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a module that contains a concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenates str1 of type str
+    and str2 of type str and
+    returns a concatenated str
+    """
+    return str1 + str2
