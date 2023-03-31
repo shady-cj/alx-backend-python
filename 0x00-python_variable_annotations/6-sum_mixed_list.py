5cd358d66bfe2bb8a570dd025ff97ea9aeb02743
shady-cj <petersp2000@gmail.com>
2023-03-31 12:05:29 +0100

    implemented task #6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a module that contains a sum_mixed_list function
+"""
+
+
+def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+    """
+    returns the sum of mxd_list of ints
+    and floats as a float
+    """
+    return sum(mxd_lst)
