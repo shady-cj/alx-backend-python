84300b83b8fe72c22c2a0be47ae6905fe2b441c7
shady-cj <petersp2000@gmail.com>
2023-03-31 11:37:46 +0100

    implementing task #0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,9 @@ a module that contains an add function
 
 
 def add(a: float, b: float) -> float:
+    """
+    adds a of type float
+    and b of tyoe float and
+    return a float
+    """
     return a + b
