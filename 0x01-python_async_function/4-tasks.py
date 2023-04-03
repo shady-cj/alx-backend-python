fb06d1eac40e328db6c947e19caeae36aed3d015
peter <petersp2000@gmail.com>
2023-04-03 22:25:39 +0100

    implementing task #4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Re-Implementing the wait_n function from 1-concurrent_coroutines.py
+to call task_wait_random from 3-tasks.py
+"""
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Implemented wait_n that returns a list of delays returned from
+    task_wait_random and the sorting the list_of_delays list using the famous
+    bubble sort.
+    """
+    list_of_delay_tasks = []
+    for i in range(n):
+        delay = task_wait_random(max_delay)
+        list_of_delay_tasks.append(delay)
+    
+    list_of_delays = await asyncio.gather(*list_of_delay_tasks)
+    for j in range(len(list_of_delays)):
+        for k in range(j, len(list_of_delays)):
+            if list_of_delays[j] > list_of_delays[k]:
+                list_of_delays[j], list_of_delays[k] = \
+                    list_of_delays[k], list_of_delays[j]
+    return list_of_delays
--- 0x01-python_async_function/README.md
+++ 0x01-python_async_function/README.md
@@ -110,7 +110,7 @@ bob@dylan:~$ ./2-main.py
 ### 3. Tasks
 
 
-Import wait_random from 0-basic_async_syntax.
+Import `wait_random` from `0-basic_async_syntax`.
 
 Write a function (do not create an async function, use the regular function syntax to do this) `task_wait_random` that takes an integer `max_delay` and returns a `asyncio.Task`.
 
@@ -137,3 +137,29 @@ bob@dylan:~$ ./3-main.py
 
 
 **File**: `3-tasks.py`
+
+
+
+
+### 4. Tasks
+Take the code from `wait_n` and alter it into a new function `task_wait_n`. The code is nearly identical to `wait_n` except `task_wait_random` is being called.
+
+
+
+```
+bob@dylan:~$ cat 4-main.py
+#!/usr/bin/env python3
+
+import asyncio
+
+task_wait_n = __import__('4-tasks').task_wait_n
+
+n = 5
+max_delay = 6
+print(asyncio.run(task_wait_n(n, max_delay)))
+
+bob@dylan:~$ ./4-main.py
+[0.2261658205652346, 1.1942770588220557, 1.8410422186086628, 2.1457353803430523, 4.002505454641153]
+```
+
+**File**: `4-tasks.py`
