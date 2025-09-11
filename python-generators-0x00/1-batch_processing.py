184aa5f68447f21616ce8d0003953a811809f202
shady-cj <petersp2000@gmail.com>
2025-09-11 07:19:33 +0100

    added batch processing feature

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,10 +1,16 @@
 from itertools import islice
 
-stream_users = __import__("0-stream_users").stream_users
+
+seed = __import__("seed")
 
 
 def stream_users_in_batches(batch_size):
-    for user in islice(stream_users(), 0, batch_size):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM user_data WHERE age > 25;")
+
+    for user in islice(cursor.fetchall(), 0, batch_size):
         yield user
 
 
