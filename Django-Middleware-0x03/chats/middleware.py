6c3b764400bfa0ffd4ca5b7a042bce1c14d42ccb
peter <petersp2000@gmail.com>
2025-09-27 18:35:32 +0100

    restrict ips

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -27,3 +27,37 @@ class RestrictAccessByTimeMiddleware:
             return HttpResponse("Forbidden hours",status=403)
         response = self.get_response(request)
         return response
+
+
+class OffensiveLanguageMiddleware:
+    def __init__(self, get_response):
+        self.ips = {}
+        self.get_response = get_response
+
+    def __call__(self, request):
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]  # Take the first IP in the list
+        else:
+            ip = request.META.get("REMOTE_ADDR")
+        if ip in self.ips:
+
+            # check the number of requests
+            time_of_first_request = self.ips[ip]["time_of_first_request"]
+            num_of_request = self.ips[ip]["num_request"]
+            now = datetime.datetime.now()
+            time_diff = now - time_of_first_request 
+            if time_diff.seconds < 60:
+                if num_of_request >= 5:
+                    return HttpResponse('Exceeded number of messages in a minute', status=403)
+                else:
+                    self.ips[ip]["num_request"] += 1
+                    response = self.get_response(request)
+                    return response
+        now = datetime.datetime.now()
+        self.ips[ip] = {"time_of_first_request": now, "num_request": 1 }
+        response = self.get_response(request)
+        return response
+            
+
+        
--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -52,7 +52,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
-    'chats.middleware.RestrictAccessByTimeMiddleware'
+    'chats.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.OffensiveLanguageMiddleware'
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
