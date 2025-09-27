77acdaf400fe252e407b86e99b2cf5e21ebe5e29
peter <petersp2000@gmail.com>
2025-09-27 17:59:08 +0100

    added time restriction middlware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,4 +1,5 @@
 import datetime
+from django.http import HttpResponse
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -13,4 +14,16 @@ class RequestLoggingMiddleware:
         log = f"{now} - User: {user} - Path: {path}\n"
         with open("requests.log", "a") as f:
             f.write(log)
+        return response
+    
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        now = datetime.datetime.now()
+        if not (now.hour >= 18 and now.hour <= 21):
+            return HttpResponse("Forbidden hours",status=403)
+        response = self.get_response(request)
         return response
