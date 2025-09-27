8fb6c3d0d6c9fecedfc1e336aa9ecd2fb465fb81
peter <petersp2000@gmail.com>
2025-09-27 15:59:00 +0100

    custom pagination

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,15 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
-    page_size = 20
-    page_query_param = 'page_size'
-    max_page_size = 100
+ 
+
+    def get_paginated_response(self, data):
+        return Response({
+            'total_items': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'next_page': self.get_next_link(),
+            'previous_page': self.get_previous_link(),
+            'items': data
+        })
